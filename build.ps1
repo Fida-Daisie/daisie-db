bda2795a6b33ea126083e11ed0bff1332e41e0a1
Fida-Daisie <daisie@fida.de>
2022-08-31 13:30:23 +0200

    added Windows support

---
+++ build.ps1
@@ -0,0 +1,2 @@
+docker build -t daisie-postgres-image .
+docker volume create daisie-postgres
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 docker build -t daisie-postgres-image .
-docker create volume daisie-postgres
+docker volume create daisie-postgres
